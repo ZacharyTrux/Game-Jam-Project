133248fb107550c824b3ed3c86117c824b840a2a
JacobPace <127350081+JacobPace@users.noreply.github.com>
2026-04-10 18:33:01 -0500

    Player Script

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Singleton Instance, persistent through scenes
+    public static Player Instance { get; private set; }
+
+    // Public Fields
+
+
+    // Private Fields
+    private InputSystem_Actions playerInput;
+
+    private void Awake()
+    {
+        playerInput = new();
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerInput.Enable();
+        playerInput.Player.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ResetPlayer()
+    {
+
+    }
+
+    public void ControlEnemies()
+    {
+
+    }
+
+    public void EnableInput()
+    {
+        playerInput.Enable();
+        playerInput.Player.Enable();
+    }
+    public void DisableInput()
+    {
+        playerInput.Disable();
+        playerInput.Player.Disable();
     }
 }
