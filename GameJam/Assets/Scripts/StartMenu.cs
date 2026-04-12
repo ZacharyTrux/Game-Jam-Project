1bfbce40ae181b9431fc5c4d2a9e98ae92fa4f75
Sarun Shrestha <sarunshrestha03@gmail.com>
2026-04-11 19:24:58 -0500

    Sound Menu

--- GameJam/Assets/Scripts/StartMenu.cs
+++ GameJam/Assets/Scripts/StartMenu.cs
@@ -1,30 +1,74 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class StartMenu : MonoBehaviour
 {
-    // Set in inspector
     [SerializeField] private GameObject[] menus;
 
+    [Header("Sound Controls")]
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+    [SerializeField] private TextMeshProUGUI muteButtonText;
+
+    private bool isMuted = false;
+
+    void Start()
+    {
+        if (SoundManager.Instance != null)
+        {
+            if (musicVolumeSlider != null)
+                musicVolumeSlider.value = SoundManager.Instance.musicVolume;
+            if (sfxVolumeSlider != null)
+                sfxVolumeSlider.value = SoundManager.Instance.sfxVolume;
+        }
+    }
+
     public void ToggleMenu(int menuIndex) => menus[menuIndex].SetActive(!menus[menuIndex].activeSelf);
-    
 
     public void CloseMenus()
     {
         foreach (GameObject menu in menus)
-        {
             menu.SetActive(false);
-        }
     }
 
-    // Start Menu funcs
     public void StartGame()
     {
-        SceneManager.LoadScene("Test1"); // Replace this with level 1
+        SceneManager.LoadScene("Test2");
     }
+
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OnMusicVolumeChanged(float value)
+    {
+        SoundManager.Instance?.SetMusicVolume(value);
+    }
+
+    public void OnSFXVolumeChanged(float value)
+    {
+        SoundManager.Instance?.SetSFXVolume(value);
+    }
+
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+
+        if (isMuted)
+        {
+            SoundManager.Instance?.SetMusicVolume(0f);
+            SoundManager.Instance?.SetSFXVolume(0f);
+            if (muteButtonText != null) muteButtonText.text = "Unmute";
+        }
+        else
+        {
+            SoundManager.Instance?.SetMusicVolume(musicVolumeSlider.value);
+            SoundManager.Instance?.SetSFXVolume(sfxVolumeSlider.value);
+            if (muteButtonText != null) muteButtonText.text = "Mute";
+        }
     }
 }
