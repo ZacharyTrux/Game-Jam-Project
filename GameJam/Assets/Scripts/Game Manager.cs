f1346e4d2fccd55e87c228d4817655edaddf4d06
Sarun Shrestha <sarunshrestha03@gmail.com>
2026-04-12 15:05:13 -0500

    GameManager console fixed

--- GameJam/Assets/Scripts/Game Manager.cs
+++ GameJam/Assets/Scripts/Game Manager.cs
@@ -3,50 +3,63 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour{
-    public static GameManager Instance {get; private set;}
-    public int CurrWave { get => _currWave;
-            set { _currWave = value;
-                if(waveText != null){
-                    waveText.text = "Wave: " + _currWave;
-                }
-            }
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public int CurrWave
+    {
+        get => _currWave;
+        set
+        {
+            _currWave = value;
+            if (waveText != null)
+                waveText.text = "Wave: " + _currWave;
         }
+    }
 
     private int _currWave;
+    private int defaultNextWave = 20;
     private float spawnRateMultiplier = 1.15f;
     private float healthPoolIncrease = 1.25f;
     private float maxEnemyMultiplier = 1.25f;
     public TextMeshProUGUI waveText;
-    
-    void Awake(){
-        if (Instance == null) {
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
-        else {
+        else
+        {
             Destroy(gameObject);
         }
     }
 
-    void Start(){
+    void Start()
+    {
         Debug.Log(waveText.text);
         CurrWave = 1;
         Time.timeScale = 1f;
         Player.Instance.EnableInput();
     }
 
-    // Update is called once per frame
-    void Update(){
-        if(XPManager.Instance != null && XPManager.Instance.TotalKills > defaultNextWave){
+    void Update()
+    {
+        if (XPManager.Instance != null && XPManager.Instance.TotalKills > defaultNextWave)
+        {
             CurrWave++;
             if (CurrWave % 5 == 0) LevelUpScreen.Instance.ScaleToWave();
-            if (CurrWave == 21 && SceneManager.GetActiveScene().name != "EndlessMode") SceneManager.LoadScene("MindPalace");
+            if (CurrWave == 21 && SceneManager.GetActiveScene().name != "EndlessMode")
+                SceneManager.LoadScene("MindPalace");
             SetupNextWave();
         }
     }
 
-    private void SetupNextWave(){
+    private void SetupNextWave()
+    {
         EnemySpawnManager.Instance.IncreaseStats(healthPoolIncrease, spawnRateMultiplier);
         defaultNextWave += XPManager.Instance.TotalKills + (int)(defaultNextWave * maxEnemyMultiplier);
     }
